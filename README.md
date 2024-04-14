8bee6cd2bfd5bdf6a15785338fafa0a3503be394
石博文 <337634268@qq.com>
2024-04-14 21:58:12 +0800

    添加移动到导航容器功能，添加readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+## 简介
+
+本项目是为了重构[OUTMAKER](https://theoutmaker.com.au/)网站，将网站从React SPA模式迁移到Next.js。由于SPA的特性，页面首次加载速度慢，同时不利于SEO，为了解决这个问题我调研了NextJS和Astro,分别运行了demo进行对比，
+Astro开发速度并不如NextJS，在交互性上Astro依然依赖客户端组件，这和NextJS一样，在性能方面，两者并没有很大差别。总之，如果你的网站是一个纯粹的静态网站，那么Astro是更好的选择，如果你的网站有一定的交互性，那么NextJS是更好的选择。
+
+## 特色
+
+为了提高网页加载速度，尽可能多的使用服务端组件。在需要交互或者有动效的情况下，尽量使用css来替代js，例如导航栏都是由css控制的，没有用到一行js代码。
+在无法避免交互的情况下，例如点击加入购物车按钮，再使用客户端组件进行渲染。
+
+## 技术选型
+
+* [Next.js](https://nextjs.org/)
+* [Tailwind CSS](https://tailwindcss.com/)
+* [daisyui](https://daisyui.com/)
+* [React Icons](https://react-icons.github.io/react-icons/)
+* [shadcn/ui](https://ui.shadcn.com/)
+
+很多人会有疑问会什么没有选择TypeScript。当你的项目十分庞大，需要很多人互相配合时，那么使用TypeScript无疑会减少大量的沟通成本，另外一些类型错误，在编译阶段就能发现，这会减少很多调试时间。或者你的项目是一个非常底层的，很多人都需要依赖的项目，使用TypeScript会极大的降低运行风险。然而NEXTOUTMAKER并不是这样，它非常简单，代码量也不多，仅仅由我一人开发。我不希望在配置TypeScript和声明各种各样的Interface上花费时间，要知道它可能会占用我一半的开发时间，这太不划算了。
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- components/header/index.jsx
+++ components/header/index.jsx
@@ -4,10 +4,13 @@ import Collection from "./collection";
 import Category from "./category";
 import Learn from "./learn";
 import DesignHelp from "./designHelp";
+import { IoCloseSharp, IoMenuSharp } from "react-icons/io5";
+
 const Header = async () => {
     return (
-        <div className="navbar bg-base-100 p-0 items-stretch">
-            <div className="navbar-start flex relative gap-6 ">
+        <div className="navbar bg-base-100 p-0 items-stretch px-4 md:px-0">
+            <div className="navbar-start hidden md:flex relative gap-6 ">
+
                 <div className="h-full dropdown dropdown-hover static group flex items-center">
                     <div role="button" className="flex gap-1 items-center">
                         Collection
@@ -44,9 +47,21 @@ const Header = async () => {
                         <FaAngleDown className="group-hover:rotate-180 transition-all duration-300 ease-in-out"></FaAngleDown>
                     </div>
                     <ul className="top-[100%] shadow-lg dropdown-content z-[1] menu  bg-base-100 w-[1600px] absolute left-0">
-                        <DesignHelp/>
+                        <DesignHelp />
                     </ul>
                 </div>
+
+            </div>
+            <div className="navbar-start md:hidden">
+
+                <label className="swap swap-rotate text-3xl ">
+                    <input type="checkbox" className="peer" />
+                    <div className="swap-on"><IoCloseSharp /></div>
+                    <div className="swap-off"><IoMenuSharp /></div>
+                    <div className="absolute top-[100%] shadow-lg origin-top transition-all  duration-200 ease-in-out  z-[1]  opacity-0 scale-95 peer-checked:opacity-100 peer-checked:scale-100 pointer-events-none peer-checked:pointer-events-auto">
+                        xxxxxx
+                    </div>
+                </label>
             </div>
             <div className="navbar-center">
                 <a className="items-center flex gap-1 items-center-ghost text-xl">daisyUI</a>
